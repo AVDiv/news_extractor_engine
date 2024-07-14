d7c3570cd871a0e73073b077b43f0e263cdc30db
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2024-07-14 12:45:25 +0530

    Implemented rss feed fetching for testing purposes (temporary)

---
+++ news_extractor_engine/main.py
@@ -0,0 +1,95 @@
+import builtins
+import os
+import logging
+import asyncio
+from time import sleep
+from typing import Coroutine
+
+import polars as pl
+from dotenv import load_dotenv
+
+from news_extractor_engine.engine.feed import FeedReader, FeedReaderData
+from news_extractor_engine.model import ArticleSource
+from news_extractor_engine.utils.devtools import dev_mode
+from news_extractor_engine.utils.discord_tools import DiscordLogger
+
+load_dotenv()
+
+ENVIRONMENT = os.getenv("ENVIRONMENT")
+DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+
+builtins.ENVIRONMENT = ENVIRONMENT
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+feed_list: list[FeedReader] = []
+worker_list: list[asyncio.Task] = []
+discord = DiscordLogger(DISCORD_WEBHOOK_URL)
+
+async def create_feed_reader_task(feed: FeedReader):
+  while True:
+    print(f"Checking for new articles from {feed.source.name}")
+    try:
+      feed_data = await feed.get_feed()
+      if feed_data.has_updated_since_last_request:
+        discord.send_embed(
+          title=f"New article from {feed.source.name}",
+          description=f"**{feed_data.feed['entries'][0]['title']}**\n{feed_data.feed['entries'][0]['summary']}",
+          url=str(feed_data.feed['entries'][0]['link']),
+          color=0xddcfee
+        )
+    except Exception as e:
+      logging.error(e)
+      discord.send_error(e)
+      return
+    await asyncio.sleep(10)
+
+def is_source_valid(data: FeedReaderData):
+  if data.feed['bozo'] != False:
+    return False
+  return True
+
+
+async def main():
+  current_directory = os.path.dirname(__file__)
+  news_sources = pl.read_csv(f"{current_directory}/config/news_sources.csv")
+  with dev_mode():
+    feed_check_list = []
+    print(news_sources[1:10])
+  for i, row in enumerate(news_sources[1:10].iter_rows()):
+    source = ArticleSource(
+      id=i,
+      name=row[2],
+      domain=row[0],
+      rss_url=row[1],
+      categories=list(row[3])
+    )
+    rss_feed = FeedReader(source)
+    feed_list.append(rss_feed)
+    with dev_mode():
+      try:
+        data = await rss_feed.get_feed()
+        feed_check_list.append(f"{source.name}: {'✅' if is_source_valid(data) else '❌'}")
+      except Exception as e:
+        feed_check_list.append(f"{source.name}: {e.__class__.__name__}")
+        logging.error(e)
+
+  for feed in feed_list:
+    task = asyncio.create_task(create_feed_reader_task(feed), name=feed.source.name)
+    worker_list.append(task)
+  discord.send_embed(title="Articles status:", description="\n".join(feed_check_list))
+  await asyncio.wait(worker_list)
+
+async def destruct():
+  for worker in worker_list:
+    worker.cancel()
+  loop.stop()
+
+if __name__ == "__main__":
+  try:
+    loop.run_until_complete(main())
+  except Exception as e:
+    logging.error(e)
+  finally:
+    asyncio.run(destruct())
