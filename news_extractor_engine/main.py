900cd23840bfe838f2f10a5243ee9f6f26cd3bf2
Avin Divakara <67309607+AVDiv@users.noreply.github.com>
2024-07-17 17:37:00 +0530

    Switched source list from CSV to MongoDB

--- news_extractor_engine/main.py
+++ news_extractor_engine/main.py
@@ -3,10 +3,13 @@ import os
 import logging
 import asyncio
 from time import sleep
+import markdownify
 from typing import Coroutine
 
 import polars as pl
 from dotenv import load_dotenv
+from pymongo import MongoClient
+from datetime import datetime
 
 from news_extractor_engine.engine.feed import FeedReader, FeedReaderData
 from news_extractor_engine.model import ArticleSource
@@ -17,15 +20,38 @@ load_dotenv()
 
 ENVIRONMENT = os.getenv("ENVIRONMENT")
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+MONGO_CONFIG_DB_CONN_STRING = os.getenv("MONGODB_CONFIG_DB_CONNECTION_STRING")
 
 builtins.ENVIRONMENT = ENVIRONMENT
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
+
 feed_list: list[FeedReader] = []
 worker_list: list[asyncio.Task] = []
 discord = DiscordLogger(DISCORD_WEBHOOK_URL)
+sources_db = MongoClient(MONGO_CONFIG_DB_CONN_STRING).get_default_database()
+sources_collection = sources_db.get_collection("source")
+sources_pipeline = [
+      {
+        '$match': {
+          'is_enabled': True
+        }
+      },
+      {
+        '$project':
+          {
+            'title': 1,
+            'rss': 1,
+            'domain': 1,
+            'channels': 1
+          }
+      },
+      {
+        '$limit': 5
+      }
+    ]
 
 async def create_feed_reader_task(feed: FeedReader):
   while True:
@@ -34,14 +60,13 @@ async def create_feed_reader_task(feed: FeedReader):
       feed_data = await feed.get_feed()
       if feed_data.has_updated_since_last_request:
         discord.send_embed(
-          title=f"New article from {feed.source.name}",
-          description=f"**{feed_data.feed['entries'][0]['title']}**\n{feed_data.feed['entries'][0]['summary']}",
+          title=f"## **{feed.source.name}**",
+          description=f"**{feed_data.feed['entries'][0]['title']}**\n{markdownify.markdownify(feed_data.feed['entries'][0]['summary'])}",
           url=str(feed_data.feed['entries'][0]['link']),
           color=0xddcfee
         )
     except Exception as e:
       logging.error(e)
-      discord.send_error(e)
       return
     await asyncio.sleep(10)
 
@@ -53,17 +78,18 @@ def is_source_valid(data: FeedReaderData):
 
 async def main():
   current_directory = os.path.dirname(__file__)
-  news_sources = pl.read_csv(f"{current_directory}/config/news_sources.csv")
+  # news_sources = pl.read_csv(f"{current_directory}/config/news_sources.csv")
+  news_sources = sources_db.source.aggregate(sources_pipeline)
   with dev_mode():
     feed_check_list = []
-    print(news_sources[1:10])
-  for i, row in enumerate(news_sources[1:10].iter_rows()):
+    print(news_sources)
+  for doc in news_sources:
     source = ArticleSource(
-      id=i,
-      name=row[2],
-      domain=row[0],
-      rss_url=row[1],
-      categories=list(row[3])
+      id=doc['_id'],
+      name=doc['title'],
+      domain=doc['domain'],
+      rss_url=doc['rss'],
+      categories=doc['channels']
     )
     rss_feed = FeedReader(source)
     feed_list.append(rss_feed)
@@ -89,7 +115,13 @@ async def destruct():
 if __name__ == "__main__":
   try:
     loop.run_until_complete(main())
+  except KeyboardInterrupt as e:
+    webhook = discord.send_embed(title="Terminating...", description=f"Shutting down the news extractor engine...")
+    sleep(3)
+    logging.error(e)
   except Exception as e:
+    webhook = discord.send_embed(title="Error", description=f"An error occurred while running the news extractor engine:\n```{e}```")
     logging.error(e)
   finally:
     asyncio.run(destruct())
+    discord.send_embed(title="Terminated", description=f"The news extractor engine was shutdown <t:{int(datetime.now().timestamp())}:R>.", webhook=webhook)
